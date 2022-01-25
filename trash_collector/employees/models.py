1541f2f0ca052cdb1f27c2e871edc24e228b3e32
dstoffels <dan.stoffels@gmail.com>
2022-01-25 14:48:08 -0500

    implemented employee model, authentication redirect, create view

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50)
+  user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+  zip_code = models.CharField(max_length=5)
