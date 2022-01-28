ec60c985fab0587767fec74a47d4b92851263aad
Lizzy <Lizzy@Lizzys-MacBook-Air.local>
2022-01-28 09:13:16 -0500

    Test commit.

--- trash_collector/static/checkout.js
+++ trash_collector/static/checkout.js
@@ -1,4 +1,4 @@
-
+console.log("hello_world")
 // This is a public sample test API key.
       // Don’t submit any personally identifiable information in requests made with this key.
       // Sign in to see your own test API key embedded in code samples.
