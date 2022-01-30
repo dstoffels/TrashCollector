127d8c4d0509939f16b33c13323674ed7bfc46b4
Lizzy <Lizzy@Lizzys-MacBook-Air.local>
2022-01-30 17:04:14 -0500

    Adjusted styling for customer index.

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -2,15 +2,13 @@ from django.urls import path
 
 from . import views
 
-# TODO: Determine what distinct pages are required for the user stories, add a path for each in urlpatterns
-
 app_name = "employees"
 urlpatterns = [
     path('', views.index, name="index"),
     path('new/', views.create, name="create"),
     path('edit_profile/', views.edit_profile, name="edit_profile"),
-    path('confirm_pickup/<int:id>/<str:selected_date>', views.confirm_pickup, name="confirm_pickup"),
+    path('<int:id>/<str:selected_date>/confirm_pickup/', views.confirm_pickup, name="confirm_pickup"),
     path('select_day/<str:day>/', views.select_day, name="select_day"),
-    path('details/<int:id>/', views.customer_details, name="details"),
+    path('<int:id>/details/', views.customer_details, name="details"),
     path('toggle_pickups/<str:day>', views.toggle_pickups, name='toggle_pickups')
 ]
