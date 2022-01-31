5c1fee458b87a4dd5c11c4c8ef1d0ae99b2a48f6
dstoffels <dan.stoffels@gmail.com>
2022-01-30 22:12:05 -0500

    BUGFIX: updated date parser for weekday selector. Added employee creattion validation to form

--- trash_collector/employees/helpers.py
+++ trash_collector/employees/helpers.py
@@ -1,13 +1,13 @@
-from datetime import date, datetime
+import datetime
 
-def parse_date(day) -> date:
+def parse_date(day) -> datetime.date:
     weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
     if day in weekdays:
-        todays_date = date.today()
+        todays_date = datetime.date.today()
         todays_index = todays_date.weekday()
         day_index = weekdays.index(day)
         difference = day_index - todays_index
-        return date(todays_date.year, todays_date.month, todays_date.day + difference)
+        return todays_date + datetime.timedelta(days=difference)
     else:
-        return datetime.strptime(day, '%Y-%m-%d').date()
+        return datetime.datetime.strptime(day, '%Y-%m-%d').date()
